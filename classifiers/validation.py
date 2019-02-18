b33f69d05d57392aa2b90f459772c30418560081
rickeydas <karam.shine@gmail.com>
2019-02-18 11:31:39 +0530

    Replaced f1_score with matthews_corrcoef

--- classifiers/validation.py
+++ classifiers/validation.py
@@ -1,21 +1,23 @@
 import numpy as np
 from tensorflow.keras.callbacks import Callback
-from sklearn.metrics import f1_score
+from sklearn.metrics import matthews_corrcoef
 from controller.saving import models_dir
 
 
 # check validation score and save intent model
 class Metrics(Callback):
     def __init__(self):
-        self.val_f1s = []
+        self.mccs = []
 
 
     def on_epoch_end(self, epoch, logs={}):
         val_predict = np.argmax(np.asarray(self.model.predict([self.validation_data[0]])), axis=-1)
         val_targ = np.argmax(self.validation_data[1], axis=-1)
-        val_f1 = f1_score(val_targ.tolist(), val_predict.tolist(), average='micro')
-        self.val_f1s.append(val_f1)
-        if len(self.val_f1s) > 1:
-            if val_f1 >= max(self.val_f1s[:-1]):
+        mcc = matthews_corrcoef(val_targ.tolist(), val_predict.tolist())
+        self.mccs.append(mcc)
+        if len(self.mccs) > 1:
+            if mcc >= max(self.mccs[:-1]):
                 self.model.save(models_dir + '/model.h5')
+        else:
+            self.model.save(models_dir + '/model.h5')
         return
